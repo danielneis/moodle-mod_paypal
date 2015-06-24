c0aaca67a2626a8633bf0be6625f41a74b288e64
Daniel Neis Araujo <danielneis@gmail.com>
2015-06-24 14:12:31 -0300

    removing unused capability

--- db/access.php
+++ db/access.php
@@ -70,13 +70,4 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
-
-    'mod/paypal:submit' => array(
-        'riskbitmask' => RISK_SPAM,
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_MODULE,
-        'legacy' => array(
-            'student' => CAP_ALLOW
-        )
-    ),
 );
